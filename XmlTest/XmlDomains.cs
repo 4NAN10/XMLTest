1ff3f232d856b4b16d011e04f4188fe615d89bc7
ryohei <oota.works@gmail.com>
2019-02-11 14:31:02 +0900

    新規追加

---
+++ XmlTest/XmlDomains.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlTest
+{
+    /// <summary>
+    /// ドメイン一覧(ルート)
+    /// </summary>
+    [System.Xml.Serialization.XmlRoot("Domains")]
+    public class XmlRoot
+    {
+        [System.Xml.Serialization.XmlElement("Domain")]
+        public List<XmlDomain> Domains { get; set; } = new List<XmlDomain>();
+    }
+
+    /// <summary>
+    /// ドメイン
+    /// </summary>
+    public class XmlDomain
+    {
+        [System.Xml.Serialization.XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [System.Xml.Serialization.XmlElement("Files")]
+        public XmlFiles Files { get; set; }
+
+        public string Read(string name)
+        {
+            return this.Name;
+        }
+    }
+
+    /// <summary>
+    /// ファイル一覧
+    /// </summary>
+    public class XmlFiles
+    {
+        [System.Xml.Serialization.XmlElement("File")]
+        public List<XmlFile> Files { get; set; } = new List<XmlFile>();
+    }
+
+    /// <summary>
+    /// ファイル
+    /// </summary>
+    public class XmlFile
+    {
+        [System.Xml.Serialization.XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [System.Xml.Serialization.XmlElement("item")]
+        public List<XmlItem> Items = new List<XmlItem>();
+
+        public string Read()
+        {
+            return this.Name;
+        }
+    }
+
+    /// <summary>
+    /// アイテム
+    /// </summary>
+    public class XmlItem
+    {
+        [System.Xml.Serialization.XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [System.Xml.Serialization.XmlAttribute("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ XmlTest/XmlTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlTest
+{
+    class XmlTest
+    {
+        static void Main(string[] args)
+        {
+            // XMLファイルの読み込み準備
+            System.IO.FileStream fs = new System.IO.FileStream(@"C:\Users\Ryohei\source\repos\XmlTest\XMLFile1.xml", System.IO.FileMode.Open);
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(XmlRoot));
+            XmlRoot groupingXml = (XmlRoot)serializer.Deserialize(fs);
+
+            // <Domains>(ルート)はListで<Domain>を保有
+            // ループで<Dmain>を1つ取得
+            foreach (XmlDomain Domain in groupingXml.Domains)
+            {
+                Console.WriteLine(Domain.Name);
+
+                // <Domain>は<Files>を単体で取得
+                // インスタンスを生成する
+                XmlFiles Files = Domain.Files;
+
+                // <Files>はListで<File>を保有
+                // ループで<File>を1つ取得
+                foreach (XmlFile File in Files.Files)
+                {
+                    Console.WriteLine(File.Name);
+                    // <File>はListで<Item>を保有
+                    // ループで<item>を1つ取得
+                    foreach (XmlItem item in File.Items)
+                    {
+                        Console.WriteLine(String.Format("name= {0} : value= {1}", item.Name, item.Value));
+                    }
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
